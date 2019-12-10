95f31fa80cb77702f06ba19637e0335b47a60af6
rcmto <rick.mmartins3@gmail.com>
2019-12-09 23:55:10 -0300

    primeiro commit

---
+++ my-app/src/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+import '../Utils/bootstrap.min.css'
+
+const Header = () => (
+    <div>
+        <header id="main-header">Hello World</header>
+        <div>TESTE DOIS</div>
+    </div>
+);
+
+export default Header;
